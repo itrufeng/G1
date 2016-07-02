56a9bf9d472ac4e223e0d74e919abc7960d7e3b8
Steve <itrufeng@gmail.com>
2016-07-02 14:39:49 +0800

    给POS机设置商品信息。

--- spec/jasmine_examples/POSSpec.js
+++ spec/jasmine_examples/POSSpec.js
@@ -1,5 +1,36 @@
 describe('POS', function(){
   var POS = require('../../lib/jasmine_examples/POS');
+  var info = [
+    {
+      barcode: 'ITEM000001',
+      name: '可口可乐',
+      unit: '瓶',
+      category: '食品',
+      subCategory: '碳酸饮料',
+      price: 3.00
+    },{
+      barcode: 'ITEM000002',
+      name: '脉动',
+      unit: '瓶',
+      category: '食品',
+      subCategory: '功能饮料',
+      price: 4.00
+    },{
+      barcode: 'ITEM000003',
+      name: '雪碧',
+      unit: '瓶',
+      category: '食品',
+      subCategory: '碳酸饮料',
+      price: 3.00
+    },{
+      barcode: 'ITEM000004',
+      name: '加多宝',
+      unit: '罐',
+      category: '食品',
+      subCategory: '凉茶饮料',
+      price: 4.00
+    }
+  ]
 
   describe('set', function(){
     it('should has been save promotions', function(){
