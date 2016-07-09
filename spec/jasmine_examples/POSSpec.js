1742aed60c54e698b73015a15beeb18c83bb2446
Troy <jizhang@thoughtworks.com>
2016-07-09 15:17:49 +1000

    处理带'-'条码的输出

--- spec/jasmine_examples/POSSpec.js
+++ spec/jasmine_examples/POSSpec.js
@@ -52,6 +52,7 @@ describe('POS', function(){
         'ITEM000001',
         'ITEM000001',
         'ITEM000001',
+        'ITEM000003-2',
         'ITEM000004',
         'ITEM000004',
         'ITEM000004'
@@ -61,7 +62,7 @@ describe('POS', function(){
       it('should return right results', ()=>{
         var pos = new POS();
         pos.setInfoes(infoes);
-        expect(pos.print(data)).toEqual('可口可乐 3.00元 5瓶 加多宝 4.00元 3罐');
+        expect(pos.print(data)).toEqual('可口可乐 3.00元 5瓶 雪碧 3.00元 2瓶 加多宝 4.00元 3罐');
       });
     });
 
@@ -73,7 +74,7 @@ describe('POS', function(){
           'ITEM000005'
         ]);
         pos.setInfoes(infoes);
-        expect(pos.print(data)).toEqual('可口可乐 3.00元 5瓶 [X] 加多宝 4.00元 3罐');
+        expect(pos.print(data)).toEqual('可口可乐 3.00元 5瓶 [X] 雪碧 3.00元 2瓶 加多宝 4.00元 3罐');
       });
     });
   });
